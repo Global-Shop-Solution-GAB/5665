255e5e2bb9ef1c2bc244e3e3b400f38281ef9adc
Service Web <sw@gssmail.com>
2024-03-14 13:55:47 -0500

    Commit changes in C:\Websites\Git\emarlow\5665\GCG_5665_CCC_CONTACT.sql

---
+++ GCG_5665_CCC_CONTACT.sql
@@ -0,0 +1,14 @@
+/* ==========================================
+ * TABLE: GCG_5665_CCC_CONTACT
+ * ========================================== */
+CREATE TABLE "GCG_5665_CCC_CONTACT"(
+ "COMPANY_CODE" CHAR(3),
+ "COMPANY_NAME" CHAR(50),
+ "CCC_CONTACT" CHAR(50),
+ "CCC_PHONE" CHAR(10),
+ "CCC_EMAIL" CHAR(100),
+ "CCC_NOTIFICATION" CHAR(15),
+ "CCC_USER" CHAR(9),
+ "CCC_DATE" CHAR(10));
+
+
