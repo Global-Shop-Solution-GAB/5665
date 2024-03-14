df3a96064911d68838254730569ab3fd28bc77d4
Service Web <sw@gssmail.com>
2024-03-14 13:55:48 -0500

    Commit changes in C:\Websites\Git\emarlow\5665\GCG_5665_CH_EXPORT.sql

---
+++ GCG_5665_CH_EXPORT.sql
@@ -0,0 +1,11 @@
+/* ==========================================
+ * TABLE: GCG_5665_CH_EXPORT
+ * ========================================== */
+CREATE TABLE "GCG_5665_CH_EXPORT"(
+ "ORDER_NO" CHAR(7),
+ "ORDER_RECORD" CHAR(4),
+ "EXPORTED" CHAR(1),
+ "LAST_USER" CHAR(9),
+ "LAST_DATE" CHAR(10));
+
+
