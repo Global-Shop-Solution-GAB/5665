f397e270708e3cbf662b8d9afc38e477398c3ee7
Service Web <sw@gssmail.com>
2024-03-14 13:55:48 -0500

    Commit changes in C:\Websites\Git\emarlow\5665\GCG_5665_POST_INFO.sql

---
+++ GCG_5665_POST_INFO.sql
@@ -0,0 +1,11 @@
+/* ==========================================
+ * TABLE: GCG_5665_POST_INFO
+ * ========================================== */
+CREATE TABLE "GCG_5665_POST_INFO"(
+ "AuthID" CHAR(20),
+ "AuthKey" CHAR(20),
+ "ContentType" CHAR(200),
+ "PostURL" CHAR(200),
+ "GSS_USER" CHAR(9));
+
+
